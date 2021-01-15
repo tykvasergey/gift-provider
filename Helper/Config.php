22145ac1ea06791cfe10dd2510b032fa000bad26
mihail <tsurkanovm@gmail.com>
2021-01-15 16:52:06 +0200

    added configuration

--- Helper/Config.php
+++ Helper/Config.php
@@ -12,7 +12,29 @@ class Config extends AbstractHelper
      */
     private $resourceConfig;
 
-    const XML_PATH_RT_BALANCE = 'real_thanks/general/balance';
+    const XML_PATH_RT_API_ENABLED = 'realthanks/api/enabled';
+    const XML_PATH_RT_API_KEY = 'realthanks/api/key';
+    const XML_PATH_RT_BALANCE = 'realthanks/general/balance';
+
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_PATH_RT_API_KEY
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApiEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(
+            self::XML_PATH_RT_API_ENABLED
+        );
+    }
 
     /**
      * @return string
