79e9baaa74f86810e9a074aedcb085b2ad2d7230
mihail <tsurkanovm@gmail.com>
2021-02-08 12:40:32 +0200

    added Associated RealThanks gifts tab to customer form with rt_order grid

---
+++ Ui/Component/Order/Listing/DataProvider.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace WiserBrand\RealThanks\Ui\Component\Order\Listing;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use WiserBrand\RealThanks\Model\ResourceModel\RtOrder\Collection as GridCollection;
+use WiserBrand\RealThanks\Model\ResourceModel\RtOrder\CollectionFactory;
+
+class DataProvider extends AbstractDataProvider
+{
+    /**
+     * @var RequestInterface $request,
+     */
+    private $request;
+
+    /**
+     * @param $name
+     * @param $primaryFieldName
+     * @param $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param RequestInterface $request
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        RequestInterface $request,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+        $this->request = $request;
+    }
+
+    public function getData(): array
+    {
+        /** @var GridCollection $collection */
+        $collection = $this->getCollection();
+        $data['items'] = [];
+        if ($this->request->getParam('email')) {
+            $collection->addFieldToFilter('email', $this->request->getParam('email'));
+            $data = $collection->toArray();
+        }
+
+        return $data;
+    }
+}
