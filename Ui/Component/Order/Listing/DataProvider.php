132e164377bd5bf85b27427b743955d8f90d3159
mihail <tsurkanovm@gmail.com>
2021-03-18 14:22:39 +0200

    fixed di:compile issues + refactoring

--- Ui/Component/Order/Listing/DataProvider.php
+++ Ui/Component/Order/Listing/DataProvider.php
@@ -3,49 +3,17 @@ declare(strict_types=1);
 
 namespace WiserBrand\RealThanks\Ui\Component\Order\Listing;
 
-use Magento\Framework\App\RequestInterface;
-use Magento\Ui\DataProvider\AbstractDataProvider;
-use WiserBrand\RealThanks\Model\ResourceModel\RtOrder\Collection as GridCollection;
-use WiserBrand\RealThanks\Model\ResourceModel\RtOrder\CollectionFactory;
-
-class DataProvider extends AbstractDataProvider
+class DataProvider extends \Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider
 {
-    /**
-     * @var RequestInterface $request,
-     */
-    private $request;
-
-    /**
-     * @param $name
-     * @param $primaryFieldName
-     * @param $requestFieldName
-     * @param CollectionFactory $collectionFactory
-     * @param RequestInterface $request
-     * @param array $meta
-     * @param array $data
-     */
-    public function __construct(
-        $name,
-        $primaryFieldName,
-        $requestFieldName,
-        CollectionFactory $collectionFactory,
-        RequestInterface $request,
-        array $meta = [],
-        array $data = []
-    ) {
-        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
-        $this->collection = $collectionFactory->create();
-        $this->request = $request;
-    }
-
     public function getData(): array
     {
-        /** @var GridCollection $collection */
-        $collection = $this->getCollection();
         if ($this->request->getParam('email')) {
-            $collection->addFieldToFilter('email', $this->request->getParam('email'));
-        }
-
-        return $collection->toArray();
+            $this->addFilter(
+                $this->filterBuilder->setField('email')
+                    ->setValue($this->request->getParam('email'))
+                    ->create()
+            );
+            }
+        return parent::getData();
     }
 }
